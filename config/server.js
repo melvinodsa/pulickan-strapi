c70b1ef72230849cf01f5518c1711a17073dc65c
Melvin Davis <melvinodsa@gmail.com>
2022-05-21 19:40:20 +0530

    postgres add

--- config/server.js
+++ config/server.js
@@ -1,7 +1,20 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      proxy: true,
+      url: env('MY_HEROKU_URL'),
+      app: {
+        keys: env.array('APP_KEYS')
+      },
+    }
+  } else {
+    return {
+      host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+      app: {
+        keys: env.array('APP_KEYS'),
+      },
+    }
+  }
+}
+
